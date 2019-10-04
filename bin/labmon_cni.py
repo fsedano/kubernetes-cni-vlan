6419e65820cd7e8fab96ac296b17ad9b6947d652
fsedanoc <fran@fransedano.net>
2019-10-04 19:33:40 +0200

    More work

--- bin/labmon_cni.py
+++ bin/labmon_cni.py
@@ -8,13 +8,16 @@ from kubernetes import client, config
 import json
 import ipaddress
 
+MASTER_INTERFACE_NAME = "ens192"
 class K8s_Params:
     def __init__(self):
         self.k8s_data = self.get_k8s_params()
         self.pod_data = self.get_pod_data()
 
+
     def get_pod_data(self):
         input_data = dict(s.split('=') for s in self.k8s_data['CNI_ARGS'].split(';'))
+        print(f"Input data is {input_data}\n")
         data = {}
         data['K8S_POD_NAME'] = input_data['K8S_POD_NAME']
         data['K8S_POD_NAMESPACE'] = input_data['K8S_POD_NAMESPACE']
@@ -28,9 +31,6 @@ class K8s_Params:
         k8s_data['CNI_IFNAME'] = os.environ['CNI_IFNAME']
         k8s_data['CNI_ARGS'] = os.environ['CNI_ARGS']
         k8s_data['CNI_PATH'] = os.environ['CNI_PATH']
-        input_data = dict(s.split('=') for s in k8s_data['CNI_ARGS'].split(';'))
-        k8s_data['K8S_POD_NAME'] = input_data['K8S_POD_NAME']
-        k8s_data['K8S_POD_NAMESPACE'] = input_data['K8S_POD_NAMESPACE']
         return k8s_data
 
     #fixme getters
@@ -50,15 +50,37 @@ class K8s_Params:
         return self.k8s_data['CNI_NETNS']
 
     def pod_name(self):
-        return ""
+        return self.pod_data['K8S_POD_NAME']
     
     def get_interface_maps(self, pod_name, netns):
         return {}
 
 class OSexec:
+    @classmethod
     def exec(self, cmd):
+        print(f"EXEC: CALLING {cmd}\n")
         rc = subprocess.check_output(cmd, shell=True).decode()
         return rc
+
+class CNIInterface:
+    def __init__(self, interface_data):
+        self.interface_data = interface_data
+
+    def bringup(self):
+        print(f"Interface data is {self.interface_data}\n")
+        vlan = self.interface_data['vlan']
+        br_name = f"phy_{vlan}"
+
+        OSexec.exec(f"modprobe --first-time 8021q")
+        OSexec.exec(f"ip link set {MASTER_INTERFACE_NAME} up")
+        OSexec.exec(f"ip link add link {MASTER_INTERFACE_NAME} name {MASTER_INTERFACE_NAME}.{vlan} type vlan id {vlan}")
+        OSexec.exec(f"ip link set {MASTER_INTERFACE_NAME}.{vlan} up")
+        OSexec.exec(f"brctl addbr {br_name}")
+        OSexec.exec(f"brctl addif {br_name} {MASTER_INTERFACE_NAME}.{vlan}")
+        OSexec.exec(f"ip link set {br_name} up")
+        OSexec.exec(f"iptables -A FORWARD -i {br_name}  -j ACCEPT")
+
+
 
 class K8s_CNI:
     def __init__(self):
@@ -70,12 +92,13 @@ class K8s_CNI:
         #rc = self.OS.exec("ls")
         #print(f"RC is {rc}")
         k = self.k
-        container_id = k.container_id()
-        print(f"pod name={k.pod_name()}")
-        print(f"pod namespace={k.netns()}")
+        #container_id = k.container_id()
         interface_data = k.get_interface_maps(k.pod_name(), k.netns())
-        print(f"Add: interface_data is {interface_data}\n")
 
+        # Loop thru the interfaces
+        for interface_data in k.get_interface_maps(k.pod_name(), k.netns()):
+            print(f"Interface is {interface_data}")
+            CNIInterface(interface_data).bringup()
 
     def oper_del(self):
         print("In oper DEL!")
--- bin/ut_cni.py
+++ bin/ut_cni.py
@@ -9,7 +9,7 @@ MOCKED_K8S_PARAMS = {
     'CNI_CONTAINERID' : "xxxx",
     'CNI_NETNS' : "33efdij",
     'CNI_IFNAME' : 'net1',
-    'CNI_ARGS' : 'ARG1=1;K8S_POD_NAME=MY_POD_NAME',
+    'CNI_ARGS' : 'K8S_POD_NAMESPACE=123455;K8S_POD_NAME=MY_POD_NAME',
     'CNI_PATH' : '/bin'
 }
 
@@ -18,17 +18,18 @@ MOCKED_INTERFACE_MAPS = [
     {"interface":"net2","vlan":520,"ip":"2.2.2.2","netmask":""}
 ]
 
-class lmUnitTest(unittest.TestCase):
+def log_exec(arg):
+    print(f"-> {arg} <-\n")
+
+class lmUnitTest(unittest.TestCase):        
     @mock.patch('labmon_cni.K8s_Params.get_k8s_params')
     @mock.patch('labmon_cni.K8s_Params.get_interface_maps')
-    @mock.patch('labmon_cni.OSexec.exec')
+    @mock.patch('labmon_cni.OSexec.exec', log_exec)
     def test_one(self,
-        labmon_cni_OSexec_exec,
         labmon_cni_K8s_Params_get_interface_maps,
         labmon_cni_K8s_Params_get_k8s_params):
         labmon_cni_K8s_Params_get_k8s_params.return_value = MOCKED_K8S_PARAMS
         labmon_cni_K8s_Params_get_interface_maps.return_value = MOCKED_INTERFACE_MAPS
-        labmon_cni_OSexec_exec.return_value = ""
         K8 = labmon_cni.K8s_CNI()
         K8.entrypoint()
         
